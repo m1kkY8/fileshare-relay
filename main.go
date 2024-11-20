616b299fcf5ed465eb2370d344dc852e017ecdaa
Milan Petrovic <5rovicmilan8@gmail.com>
2024-11-20 18:30:53 +0100

    description

--- main.go
+++ main.go
@@ -4,6 +4,12 @@ import (
 	"fileshare-relay/src/server"
 )
 
+/*
+starts tcp server on port 9001
+server accepts connections from senders and receivers
+and just acts as middle-man between two clients
+*/
+
 func main() {
 	server := server.NewServer()
 	server.Start()
